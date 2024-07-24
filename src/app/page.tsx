a9e7fe47971d9861d8de50baa60727c3c06965f6
Shi Yao Liu <shiyao_liu@apple.com>
2024-07-24 02:30:57 -0700

    Update email.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
               <a href="https://linkedin.com/in/liu6/" target="_blank">
                 <i className="fa fa-linkedin" />
               </a>
-              <a href="mailto:liusy1588@gmail.com" target="_blank">
+              <a href="mailto:shiyao3@illinois.edu" target="_blank">
                 <i className="fa fa-envelope" />
               </a>
               <a href="https://github.com/shiyao3/" target="_blank">
